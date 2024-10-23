6b76ea31e2e876152f3ce258eb07f7b98b63312a
vale_ <valeria.zerain@gmail.com>
2024-10-23 17:22:54 -0400

    primer commit

---
+++ src/test/java/segundoParcial/CreateUser.java
@@ -0,0 +1,28 @@
+package segundoParcial;
+
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class CreateUser {
+    @Test
+    @DisplayName("Create user is successfull")
+    public void createUser(){
+        JSONObject body = new JSONObject();
+        body.put("Email","valeria.parcial@qa.com");
+        body.put("FullName","Valeria Zerain");
+        body.put("Password","Pruebas123");
+
+        Response response = given()
+                .body(body.toString())
+                .post("https://todo.ly/api/items.json");
+        response.then()
+                .statusCode(200)
+                .body("Email", equalTo(body.get("Email")))
+                .log().all();
+    }
+}
